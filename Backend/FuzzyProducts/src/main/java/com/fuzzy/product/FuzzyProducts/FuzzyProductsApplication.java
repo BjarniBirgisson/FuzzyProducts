afed1068c2b02aff85088887eb339120f30df8c9
BjarniBirgisson <bjarni.birgisson86@gmail.com>
2024-11-01 18:57:58 +0100

    Initial Backend Project Setup

---
+++ Backend/FuzzyProducts/src/main/java/com/fuzzy/product/FuzzyProducts/FuzzyProductsApplication.java
@@ -0,0 +1,13 @@
+package com.fuzzy.product.FuzzyProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuzzyProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuzzyProductsApplication.class, args);
+    }
+
+}
---
+++ Backend/FuzzyProducts/src/test/java/com/fuzzy/product/FuzzyProducts/FuzzyProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuzzy.product.FuzzyProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuzzyProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
