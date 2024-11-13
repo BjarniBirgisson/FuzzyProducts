bdfd3fa411bc5b251e1fb76851dbacf28650f5b5
BjarniBirgisson <bjarni.birgisson86@gmail.com>
2024-11-13 15:53:31 +0100

    Searching and some styling

--- Frontend/FuzzyProductsFrontend/src/main.ts
+++ Frontend/FuzzyProductsFrontend/src/main.ts
@@ -1,13 +1,14 @@
 import './style.css';
 import { trigger } from './integration';
 import { setProductList } from './productList';
+import { searchProducts } from './productList';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h3>Fuzzy Products Frontend</h3>
     <div class="card">
       <label for="search">Search Products</label>
-      <input type="search" id="search" name="search"> 
+      <input type="search" id="search" name="search" placeholder="Search Products By Name"> 
     </div>
     <button id="trigger" type="button">Trigger</button>
     <div id="product-list-container"></div>
@@ -18,5 +19,7 @@ const element = document.getElementById('trigger');
 if (!element) {
   throw new Error('No element with ID `result`');
 } else {
-  element.addEventListener('click', () => setProductList());
+  element.addEventListener('click', () => searchProducts());
+  setProductList();
 }
+
--- Frontend/FuzzyProductsFrontend/src/productList.ts
+++ Frontend/FuzzyProductsFrontend/src/productList.ts
@@ -1,4 +1,5 @@
 import { getProducts } from './integration';
+import { search } from './integration';
 import { Product } from './integration';
 
 export async function setProductList() {
@@ -8,16 +9,40 @@ export async function setProductList() {
   }
 
   getProducts().then(products => 
-    result.innerHTML = products.map(p => getProductCard(p)).toString());
+    result.innerHTML = products.map(p => getProductCard(p)).join(""));
 }
 
 export function getProductCard(p: Product): String {
   var card: String = '';
-  card += '<div class="product-card">';
+  card += '<div class="product-card" id="' + p.productId + '">';
   card += '<h3 class="product-title">' + p.name + '</h3>';
-  card += '<p class="product-description">' + p.description + '</p>';
+  card += '<p class="product-category">' + p.category + '</p>';
   card += '<p class="product-price">' + p.price + '</p>';
   card += '</div>';
 
   return card;
+}
+
+export async function searchProducts() {
+  const result = document.getElementById('product-list-container');
+  const q = document.getElementById('search') as HTMLInputElement;
+  if (!result) {
+    throw new Error('No element with ID `result`');
+  }
+  if (!q) {
+    throw new Error('No element with ID `search`');
+  }
+  
+  if (isBlank(q.value)) {
+    getProducts().then(products => 
+      result.innerHTML = products.map(p => getProductCard(p)).join(""));
+  } else {
+    search(q.value).then(products => 
+      result.innerHTML = products.map(p => getProductCard(p)).join(""));
+
+  }
+}
+
+function isBlank(str: string) {
+    return (!str || /^\s*$/.test(str));
 }
